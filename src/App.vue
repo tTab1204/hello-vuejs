e2021c2505bf7b143397f3d14e64e1cc9e9c5857
이주영 <ijuyeong@ijuyeong-ui-MacBookPro.local>
2022-01-18 18:01:15 +0900

    style: button wrapper 스타일 변경

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
-    <div class="button-wrapper">
-      <!-- 버튼은 컴포넌트로 만들기 -->
-      <button
-        class="btn btn-outline-primary"
-        v-for="language in languages"
-        :key="language.type"
-        @click="changeLanguage(language)"
-      >
-        {{ language.name }}
-      </button>
-    </div>
-    <div class="text">
-      {{ $t("text") }}
+    <div class="page-container">
+      <div class="button-container">
+        <div class="button-wrapper">
+          <!-- 버튼은 컴포넌트로 만들기 -->
+          <button
+            class="btn btn-outline-primary"
+            v-for="language in languages"
+            :key="language.type"
+            @click="changeLanguage(language)"
+          >
+            {{ language.name }}
+          </button>
+        </div>
+        <div class="language-text">
+          {{ $t("text") }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -46,9 +50,37 @@ body,
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: "Pretendard";
+}
+
+.page-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 2px solid var(--bs-blue);
+  border-radius: 8px;
+  padding: 1rem;
 }
 
 .button-wrapper {
   display: flex;
+  flex-wrap: wrap;
+}
+
+.btn {
+  width: 100px;
+  margin-right: 0.8rem;
+}
+
+.language-text {
+  font-size: 1.5rem;
+  margin-top: 1.5rem;
 }
 </style>
